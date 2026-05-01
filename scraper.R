ef616baeac26dc78f916f94c4b2ff45438dc8ff0
Alex Trif <alextrif25@gmail.com>
2026-05-01 22:41:10 +0000

    aaaa

--- scraper.R
+++ scraper.R
@@ -4,7 +4,7 @@ source_python("scraper.py")
 
 # Scrape pages 1 through 5, skip listings already in master
 df <- scrape_pages(
-  start_page    = 80L,
+  start_page    = 210L,
   end_page      = 500L,
   master_path   = "master_data.csv",
   excel_path    = "cars_and_bids.xlsx",   # optional snapshot
